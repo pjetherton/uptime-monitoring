e3798500aa5c06e3e3de7c5fc7097f5507293284
Paul Etherton <paul.etherton@cloudbuy.com>
2014-11-10 12:20:44 +0000

    Fixed some bugs in the previous commit.

--- server/allow_deny.js
+++ server/allow_deny.js
@@ -1,12 +1,14 @@
 /* Copyright (c) 2014 Paul Etherton
  */
 
-URLs.allow({
-    insert: function () {
-	return true;
-    }
-
-    remove: function() {
-	return true;
-    }
+Meteor.startup(function () {
+    URLs.allow({
+	insert: function () {
+	    return true;
+	},
+	
+	remove: function() {
+	    return true;
+	}
+    });
 });
